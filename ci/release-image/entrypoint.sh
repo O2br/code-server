d48e55dec71da3c2b5ea891af2435918e24c1c34
Anmol Sethi <hi@nhooyr.io>
2020-09-03 15:41:42 -0400

    docker: Fix $DOCKER_USER
    Now we cp instead of mv $HOME as mv will fail due to the
    user not existing.
    
    We also only try to convert if the user hasn't been changed.
    
    Finally I added usage to the docker docs in install.md
    
    Closes #2056

--- ci/release-image/entrypoint.sh
+++ ci/release-image/entrypoint.sh
@@ -1,18 +1,24 @@
-#!/usr/bin/env sh
+#!/bin/sh
 set -eu
-
-if [ "${DOCKER_USER-}" ]; then
-  echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
-  sudo usermod --login "$DOCKER_USER" \
-    --move-home --home "/home/$DOCKER_USER" \
-    coder
-  sudo groupmod -n "$DOCKER_USER" coder
-
-  sudo sed -i "/coder/d" /etc/sudoers.d/nopasswd
-  sudo sed -i "s/coder/$DOCKER_USER/g" /etc/fixuid/config.yml
-  export HOME="/home/$DOCKER_USER"
-fi
 
 # This isn't set by default.
 export USER="$(whoami)"
+
+if [ "${DOCKER_USER-}" != "$USER" ]; then
+  echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd > /dev/null
+  sudo usermod --login "$DOCKER_USER" \
+    --home "/home/$DOCKER_USER" \
+    coder
+  sudo groupmod -n "$DOCKER_USER" coder
+
+  # We cannot use usermod to do this as it uses mv which will
+  # fail as files are open in $HOME. We have to copy.
+  sudo cp -a "$HOME" "/home/$DOCKER_USER"
+  export HOME="/home/$DOCKER_USER"
+  export USER="$(whoami)"
+
+  sudo sed -i "/coder/d" /etc/sudoers.d/nopasswd
+  sudo sed -i "s/coder/$DOCKER_USER/g" /etc/fixuid/config.yml
+fi
+
 dumb-init fixuid -q /usr/bin/code-server "$@"
